5282fafe9a1eee4bb82a267671f3af4a762989c6
LaurensKing <127389248+LaurensKing@users.noreply.github.com>
2024-01-10 15:16:24 +0100

    Fix

--- react_aspcore_app/ClientApp/src/pages/Contact.js
+++ react_aspcore_app/ClientApp/src/pages/Contact.js
@@ -14,7 +14,7 @@ function Contact() {
         <p>Mocht u met de trein en bus komen dan kunt u de routebeschrijving raadplegen.</p>
 
         <div class="mapContainer">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1225.2090191427894!2d5.1202948!3d52.1085222!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c66f6a619fcfe1%3A0xd04ef4a46afb9e92!2sStichting%20Accessibility!5e0!3m2!1snl!2snl!4v1704876708283!5m2!1snl!2snl"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe title="Stichting Accessibility Maps" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1225.2090191427894!2d5.1202948!3d52.1085222!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c66f6a619fcfe1%3A0xd04ef4a46afb9e92!2sStichting%20Accessibility!5e0!3m2!1snl!2snl!4v1704876708283!5m2!1snl!2snl"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
         
         <p>Algemene informatie over de toegankelijkheid, rolstoelvriendelijkheid en het meebrengen van geleidehonden staat op onze pagina over de toegankelijkheid van ons kantoor(insert link toegankelijkeid)</p>
@@ -26,7 +26,7 @@ function Contact() {
             
         <h1>Nieuwsgierig naar onze vacatures?</h1>
 
-        <p>Bekijk <a href="https://www.accessibility.nl/werken-bij-accessibility" target="_blank">op deze pagina</a> de vacatures die we momenteel open hebben staan. We bieden ook stageplekken!</p>
+        <p>Bekijk <a href="https://www.accessibility.nl/werken-bij-accessibility" target="_blank" rel="noreferrer">op deze pagina</a> de vacatures die we momenteel open hebben staan. We bieden ook stageplekken!</p>
 
         </html>
         );

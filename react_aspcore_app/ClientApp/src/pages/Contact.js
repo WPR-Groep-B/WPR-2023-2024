9367bd0b40099bdc3706d53d2c999ec87b979f16
TimothyTichelaar <21108366@student.hhs.nl>
2024-01-10 09:58:10 +0100

    Google maps iframe heeft een alternative tekst gekregen

--- react_aspcore_app/ClientApp/src/pages/Contact.js
+++ react_aspcore_app/ClientApp/src/pages/Contact.js
@@ -13,7 +13,7 @@ function Contact() {
 
         <p>Mocht je met de trein en bus komen dan kun je hier de uitgebreide routebeschrijving (insert link naar uitgebreide route beschrijving) raadplegen.</p>
 
-        <iframe src="https://www.google.com/maps/place/Stichting+Accessibility/@52.1085222,5.1202948,18.5z/data=!4m15!1m8!3m7!1s0x47c66f3b97fb2a95:0x30cf938aa1cf68a9!2sChristiaan+Krammlaan+2,+3571+AX+Utrecht!3b1!8m2!3d52.108711!4d5.1211567!16s%2Fg%2F11b8v4bc8_!3m5!1s0x47c66f6a619fcfe1:0xd04ef4a46afb9e92!8m2!3d52.1086494!4d5.1210281!16s%2Fg%2F1tgj_vkw?entry=ttu" title= "routebeschrijving">routebeschrijving</iframe>  
+        <iframe src="https://www.google.com/maps/place/Stichting+Accessibility/@52.1085222,5.1202948,18.5z/data=!4m15!1m8!3m7!1s0x47c66f3b97fb2a95:0x30cf938aa1cf68a9!2sChristiaan+Krammlaan+2,+3571+AX+Utrecht!3b1!8m2!3d52.108711!4d5.1211567!16s%2Fg%2F11b8v4bc8_!3m5!1s0x47c66f6a619fcfe1:0xd04ef4a46afb9e92!8m2!3d52.1086494!4d5.1210281!16s%2Fg%2F1tgj_vkw?entry=ttu" title= "routebeschrijving" alt="Routebeschrijving via google maps">routebeschrijving</iframe>  
         
         <p>Algemene informatie over de toegankelijkheid, rolstoelvriendelijkheid en het meebrengen van geleidehonden staat op onze pagina over de toegankelijkheid van ons kantoor(insert link toegankelijkeid)</p>
 

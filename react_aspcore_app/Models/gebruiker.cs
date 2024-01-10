1f5fd8f7f045920553f9e6ac389b5ab56cb5c796
mikavmil <gamermeganl@gmail.com>
2024-01-10 11:33:28 +0100

    database + mig

---
+++ react_aspcore_app/Models/gebruiker.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+public class gebruiker {
+
+    [Key]
+    public int GebruikerId { get; set; }
+
+    public string Voornaam { get; set;}
+
+    public string Achternaam { get; set;}
+
+    public string email { get; set;}
+
+}
+
+public class gebruikerBedrijf : gebruiker {
+
+    public string bedrijfsnaam { get; set; }
+
+}
+
+public class gebruikerDeskundige : gebruiker {
+
+    public string beperking {  get; set; }
+
+}
+
+public class gebruikerBeheerder : gebruiker {
+
+    public string functie {get; set; }
+
+}

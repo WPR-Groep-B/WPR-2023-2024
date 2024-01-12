6b3cd1c430fa437853ab9f92a07316460d769670
mikavmil <gamermeganl@gmail.com>
2024-01-12 11:20:13 +0100

    api link

--- react_aspcore_app/ClientApp/src/components/LocalLogin.js
+++ react_aspcore_app/ClientApp/src/components/LocalLogin.js
@@ -14,7 +14,7 @@ function Login() {
         if (email === "" || wachtwoord === "") {
             alert("Vul alle velden in!" + email + wachtwoord);
         }
-        axios.post('https://localhost:7251/api/user/login', {
+        axios.post('https://appservicewprgroepb.azurewebsites.net/api/user/login', {
             email: email,
             wachtwoord: wachtwoord
         }).then((response) => {

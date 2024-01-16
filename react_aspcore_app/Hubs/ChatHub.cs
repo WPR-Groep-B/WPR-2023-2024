9138418a72389a68909f36295337c04967a32ade
mikavmil <gamermeganl@gmail.com>
2024-01-16 20:03:38 +0100

    signalIr classes

---
+++ react_aspcore_app/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace react_aspcore_app.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.ChatRoom); // Add user to group
+            await Clients.Group(userConnection.ChatRoom).SendAsync("JoinRoom", userConnection.Username, $"{userConnection.UserName} has joined {userConnection.ChatRoom}"); // Send message to all clients in group
+        }
+    }
+}

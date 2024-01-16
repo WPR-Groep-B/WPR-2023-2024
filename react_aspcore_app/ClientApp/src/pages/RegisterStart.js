b551e96255265ea555270ca82951364aad45a5df
LaurensKing <127389248+LaurensKing@users.noreply.github.com>
2024-01-16 12:43:20 +0100

    register uncommenten

--- react_aspcore_app/ClientApp/src/pages/RegisterStart.js
+++ react_aspcore_app/ClientApp/src/pages/RegisterStart.js
@@ -1,74 +1,73 @@
-// import React, { useState, useEffect } from 'react';
-// import styles from '../styles/Register.module.css';
-// // import { Navlink } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/Register.module.css';
 
-// function RegisterName() {
-//     useEffect(() => {
-//         document.title = "Stichting Accessibility - Register";
-//       }, []);
-//     const [naam, setNaam] = useState('');
-//     const [anaam, setAnaam] = useState('');
+function RegisterName() {
+    useEffect(() => {
+        document.title = "Stichting Accessibility - Register";
+      }, []);
+    const [naam, setNaam] = useState('');
+    const [anaam, setAnaam] = useState('');
 
-//     const handleNaamChange = (e) => {
-//         setNaam(e.target.value);
-//     };
+    const handleNaamChange = (e) => {
+        setNaam(e.target.value);
+    };
 
-//     const handleAnaamChange = (e) => {
-//         setAnaam(e.target.value);
-//     };
+    const handleAnaamChange = (e) => {
+        setAnaam(e.target.value);
+    };
 
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-//         if (naam.trim() !== '' && anaam.trim() !== '') {
-//             window.location.href = "/register-info"
-//         } else {
-//             console.error("Please fill in all required fields");
-//         }
-//     }; 
+        if (naam.trim() !== '' && anaam.trim() !== '') {
+            window.location.href = "/register-info"
+        } else {
+            console.error("Please fill in all required fields");
+        }
+    }; 
     
-//     return (
-//         <div className={styles.body}>
-//             <div className={styles.container}>
-//                 <h1>Stichting Accessibility</h1>
-//                 <p>Gelieve hier uw Voor- en Achternaam op te geven</p>
+    return (
+        <div className={styles.body}>
+            <div className={styles.container}>
+                <h1>Stichting Accessibility</h1>
+                <p>Gelieve hier uw Voor- en Achternaam op te geven</p>
 
-//                 <hr></hr>
-//             <form className={styles.form} onSubmit={handleSubmit}>
+                <hr></hr>
+            <form className={styles.form} onSubmit={handleSubmit}>
 
-//             <div>
-//                 <label htmlFor="firstName">Voornaam:</label>
-//                 <input 
-//                     type="text" 
-//                     id="firstName" 
-//                     name="firstName" 
-//                     placeholder="Voornaam" 
-//                     required
-//                     value={naam}
-//                     onChange={handleNaamChange}
-//                     />
-//             </div>
+            <div>
+                <label htmlFor="firstName">Voornaam:</label>
+                <input 
+                    type="text" 
+                    id="firstName" 
+                    name="firstName" 
+                    placeholder="Voornaam" 
+                    required
+                    value={naam}
+                    onChange={handleNaamChange}
+                    />
+            </div>
             
-//             <div>
-//                 <label htmlFor="lastName">Achternaam:</label>
-//                 <input
-//                     type="text"
-//                     id="lastName"
-//                     name="lastName"
-//                     placeholder="Achternaam" 
-//                     required
-//                     value={anaam}
-//                     onChange={handleAnaamChange}
-//                     />
-//             </div>
+            <div>
+                <label htmlFor="lastName">Achternaam:</label>
+                <input
+                    type="text"
+                    id="lastName"
+                    name="lastName"
+                    placeholder="Achternaam" 
+                    required
+                    value={anaam}
+                    onChange={handleAnaamChange}
+                    />
+            </div>
 
-//             <hr></hr>
+            <hr></hr>
             
-//             <button className={styles.registerbtn} type="submit">Volgende</button>
-//             </form>
-//             </div>
-//         </div>
-//     );
-// }
+            <button className={styles.registerbtn} type="submit">Volgende</button>
+            </form>
+            </div>
+        </div>
+    );
+}
 
-// export default RegisterName;
+export default RegisterName;

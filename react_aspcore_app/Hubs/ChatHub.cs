5e710e1bbb725f05edd844467e9f3a9b0add0e0d
mikavmil <gamermeganl@gmail.com>
2024-01-17 00:14:14 +0100

    Add SendMessage method to ChatHub

--- react_aspcore_app/Hubs/ChatHub.cs
+++ react_aspcore_app/Hubs/ChatHub.cs
@@ -4,6 +4,12 @@ namespace react_aspcore_app.Hubs
 {
     public class ChatHub : Hub
     {
+        
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message); // Send message to all clients
+        }
+
         public async Task joinroom(UserConnection userConnection)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room); // Add user to group

37a8d5b28365d2b6506c1092f4cb5249cbb01b38
mikavmil <gamermeganl@gmail.com>
2024-01-16 22:14:36 +0100

    Refactor ChatComponent and ChatPage
    This commit refactors the ChatComponent and ChatPage files. It introduces new imports, adds form handling functionality, and implements a SignalR connection for joining chat rooms.

--- react_aspcore_app/ClientApp/src/components/ChatComponent.jsx
+++ react_aspcore_app/ClientApp/src/components/ChatComponent.jsx
@@ -1,8 +1,29 @@
-﻿function Chat() {
+﻿import { Form, Button, } from "react-bootstrap";
+import { useState } from "react";
+
+
+
+function Chat({ joinRoom }) {
+
+    const [UserName, setUserName] = useState();
+    const [Room, setRoom] = useState();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        joinRoom(UserName, Room);
+    };
+
     return (
         <div>
-            <h1>Chat</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Name</label>
+                <input type="text" onChange={(event) => setUserName(event.target.value)} />
+                <label>Room</label>
+                <input type="text" onChange={(event) => setRoom(event.target.value)} />
+                    
+                <button type="submit" disabled={!UserName || !Room}>Join</button>
+            </form>
         </div>
-    )  
+    );
 }
-export default Chat
+export default Chat;
--- react_aspcore_app/ClientApp/src/pages/ChatPage.jsx
+++ react_aspcore_app/ClientApp/src/pages/ChatPage.jsx
@@ -1,10 +1,34 @@
 ﻿import Chat from "../components/ChatComponent";
+import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
 
 function ChatPage() {
+
+    const joinRoom = async (UserName, Room) => {
+        try {
+            const connection = new HubConnectionBuilder()
+                .withUrl("https://localhost:7251/ChatHub")
+                .configureLogging(LogLevel.Information)
+                .build();
+
+            connection.on("ReceiveMessage", (user, message) => {
+                console.log(user, message);
+            })
+
+            await connection.start();
+            await connection.invoke("joinroom", { UserName: UserName, Room: Room });
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+
+    }
+
+
     return (
         <div>
             <h1>ChatPagina</h1>
-            <Chat />
+            <Chat joinRoom={joinRoom} />
         </div>
     )
 }

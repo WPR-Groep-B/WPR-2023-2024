c82250b3868aebd7a59ee5e31c0e091506a44949
mikavmil <gamermeganl@gmail.com>
2024-01-16 20:36:30 +0100

    Add ChatComponent and ChatPage

---
+++ react_aspcore_app/ClientApp/src/components/ChatComponent.jsx
@@ -0,0 +1,8 @@
+﻿function Chat() {
+    return (
+        <div>
+            <h1>Chat</h1>
+        </div>
+    )  
+}
+export default Chat
---
+++ react_aspcore_app/ClientApp/src/pages/ChatPage.jsx
@@ -0,0 +1,11 @@
+﻿import Chat from "../components/ChatComponent";
+
+function ChatPage() {
+    return (
+        <div>
+            <h1>ChatPagina</h1>
+            <Chat />
+        </div>
+    )
+}
+export default ChatPage;

51e09593fe9ac4770a656e2be0e37e3968778acc
TimothyTichelaar <21108366@student.hhs.nl>
2024-01-17 14:16:21 +0100

    rol en beperking ook toegevoegd aan de dbcontext zodat de tabellen gemaakt kunnen worden

--- react_aspcore_app/Models/SampleDBContext.cs
+++ react_aspcore_app/Models/SampleDBContext.cs
@@ -59,6 +59,12 @@ using Microsoft.EntityFrameworkCore;
 
             // Do the same for other relationships that could cause multiple cascade paths
 
+            modelBuilder.Entity<rol>()
+                .ToTable("rollen");
+
+            modelBuilder.Entity<beperking>()
+                .ToTable("beperkingen");
+
             modelBuilder.Entity<gebruiker>()
                 .ToTable("gebruikers");
 

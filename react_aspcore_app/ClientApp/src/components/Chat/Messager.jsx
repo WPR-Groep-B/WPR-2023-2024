e314b7cbc7314a1f99a930c73aaa27cf929e7226
LaurensKing <127389248+LaurensKing@users.noreply.github.com>
2024-01-18 12:58:10 +0100

    chat verbeterd

--- react_aspcore_app/ClientApp/src/components/Chat/Messager.jsx
+++ react_aspcore_app/ClientApp/src/components/Chat/Messager.jsx
@@ -1,18 +1,14 @@
 import MessageContainer from "./messageContainer"
 import SendMessageForm from "./sendMessageForm"
+import styles from '../../styles/Chat.module.css';
 
 function Chat({ messages, sendMessage, closeConnection }) {
     return (<>
         <div className="chat">
             <h3>Chat</h3>
             <hr></hr>
-            <div className="chat">
-                <button type="reset" onClick={() => closeConnection()}>Close connection</button>
-            </div>
-            <hr></hr>
             <MessageContainer messages={messages} />
-            <SendMessageForm sendMessage={sendMessage} />
-
+            <SendMessageForm sendMessage={sendMessage} closeConnection={closeConnection} />
         </div>
     </>
     )
